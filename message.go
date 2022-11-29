0b5175f073b49edea693da5f99a418a08ad96fa4
Julius Pedersen <deifyed@tutanota.com>
2022-11-29 20:15:28 +0100

    ✅ Add origin field to message

--- message.go
+++ message.go
@@ -2,13 +2,25 @@ package main
 
 import "fmt"
 
+const (
+	// typeBumpTeam is used to increase the score of a team by 1. Bump team expects "team1" or "team2" as payload.
+	typeBumpTeam = "bump-team"
+
+	// Reset is a special type that is used to reset the score to 0-0. Reset expects no payload
+	typeReset = "reset"
+)
+
+var allTypes = []string{typeBumpTeam, typeReset}
+
 type Message struct {
+	// Origin identifies the source of the event, usually an UUID
+	Origin  string `json:"origin"`
 	Type    string `json:"type"`
 	Payload string `json:"payload"`
 }
 
 func (m *Message) Validate() error {
-	if !contains([]string{typeBumpTeam, typeReset}, m.Type) {
+	if !contains(allTypes, m.Type) {
 		return fmt.Errorf("invalid type: %s", m.Type)
 	}
 
@@ -24,11 +36,3 @@ func contains(haystack []string, needle string) bool {
 
 	return false
 }
-
-const (
-	// typeBumpTeam is used to increase the score of a team by 1. Bump team expects "team1" or "team2" as payload.
-	typeBumpTeam = "bump-team"
-
-	// Reset is a special type that is used to reset the score to 0-0. Reset expects no payload
-	typeReset = "reset"
-)
